67d8b31ed856139f166f18ed21430591ac577b7c
iPrisc <florent.lelion@gmail.com>
2026-03-03 15:59:42 +0100

    done checkpoint 7

--- mfe-header/src/components/Navbar.jsx
+++ mfe-header/src/components/Navbar.jsx
@@ -7,9 +7,28 @@ function Navbar() {
   const [cartCount, setCartCount] = useState(0);
 
   useEffect(() => {
+  }, []);
+
+  useEffect(() => {
     // TODO 1 : s'abonner à game:joined → incrémenter le badge notifications
+    const onGameJoined = () => {
+      setNotifications(prev => prev + 1);
+    };
+
     // TODO 2 : s'abonner à cart:updated → mettre à jour le badge panier (count)
+    const onCartUpdated = (payload) => {
+      const count = payload?.count ?? payload?.detail?.count ?? 0;
+      setCartCount(count);
+    };
+
+    eventBus.on('game:joined', onGameJoined);
+    eventBus.on('cart:updated', onCartUpdated);
+
     // TODO 3 : retourner le cleanup des 2 abonnements
+    return () => {
+      eventBus.off('game:joined', onGameJoined);
+      eventBus.off('cart:updated', onCartUpdated);
+    };
   }, []);
 
   return (
